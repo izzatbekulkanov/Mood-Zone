ffd8cad20546dbb379f80f7cc08fb209c231bace
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2023-11-17 19:14:49 +0500

    .

--- theme/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def mainView(request):
-    return render(request, 'partials/base.html')
-def studentView(request):
-    return render(request, 'users/students.html')
