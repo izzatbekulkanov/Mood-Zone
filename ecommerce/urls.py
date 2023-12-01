a8219aca17cfd494612d4bf985277d6aff2d5a0f
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2023-12-01 14:22:56 +0500

    .

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,12 +1,29 @@
 from django.urls import path
-from .views import (dashboard, categoriesListView, vendorDashboardView, shopMainView, wishlistView, )
+from .views import (
+    dashboard,
+    categoriesListView,
+    vendorDashboardView,
+    shopMainView,
+    wishlistView,
+    userProfileView,
+    productGridView,
+    userListView,
+    orderProcessView,
+    invoiceView,
+)
+
 app_name = 'ecommerce'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('categoriesList/', categoriesListView, name='categoriesList'),
+    path('productGrid/', productGridView, name='productGrid'),
+    path('categoriesList/', categoriesListView, name='categoriesList'),
     path('vendorDashboard/', vendorDashboardView, name='vendorDashboard'),
     path('shopMain/', shopMainView, name='shopMain'),
     path('wishlist/', wishlistView, name='wishlist'),
     path('userProfile/', userProfileView, name='userProfile'),
+    path('userList/', userListView, name='userList'),
+    path('orderProcess/', orderProcessView, name='orderProcess'),
+    path('invoice/', invoiceView, name='invoice'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -7,6 +7,8 @@ def dashboard(request):
     return render(request, 'e-commerce/dashboard/main.html')
 def categoriesListView(request):
     return render(request, 'e-commerce/categories-list.html')
+def productGridView(request):
+    return render(request, 'e-commerce/products3632.html')
 def vendorDashboardView(request):
     return render(request, 'e-commerce/vendor-dashboard.html')
 def shopMainView(request):
@@ -15,3 +17,9 @@ def wishlistView(request):
     return render(request, 'e-commerce/wishlist.html')
 def userProfileView(request):
     return render(request, 'e-commerce/user-profile.html')
+def userListView(request):
+    return render(request, 'e-commerce/user-list.html')
+def orderProcessView(request):
+    return render(request, 'e-commerce/order-process.html')
+def invoiceView(request):
+    return render(request, 'e-commerce/invoice.html')
