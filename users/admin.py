ff5d1b2efaac4a233ee23007841168f09ca27aa6
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2023-12-08 15:49:16 +0500

    .

--- users/admin.py
+++ users/admin.py
@@ -1,30 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .models import CustomUser
+from django.utils.html import format_html
+from .models import CustomUser, Role
 
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'get_roles')
 
     fieldsets = (
         ('Personal Info', {'fields': (
-        'first_name', 'username', 'last_name', 'email', 'profile_image', 'profile_cover', 'phone_number', 'bio', 'birth_of_day',
+            'first_name', 'username', 'last_name', 'email', 'profile_image', 'profile_cover', 'phone_number', 'bio',
+            'birth_of_day',
         )}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'roles')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'roles'),
         }),
     )
 
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+    def get_roles(self, obj):
+        roles_html = ", ".join([role.name for role in obj.roles.all()])
+        return format_html(roles_html)
 
     def get_fieldsets(self, request, obj=None):
         if not obj:
@@ -32,6 +37,9 @@ class CustomUserAdmin(UserAdmin):
         return super().get_fieldsets(request, obj)
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(CustomUserAdmin):
-    pass
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'key')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    key = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
     username = models.CharField(max_length=30, unique=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     profile_image = models.ImageField(upload_to='profile_image')
     profile_cover = models.ImageField(upload_to='profile_cover')
     phone_number = models.CharField(max_length=13, blank=True, null=True)
@@ -15,6 +25,7 @@ class CustomUser(AbstractUser):
     verified = models.BooleanField(default=False)
     bio = models.CharField(max_length=150)
     birth_of_day = models.DateField(blank=True, null=True)
+    roles = models.ManyToManyField(Role, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
@@ -22,9 +33,8 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-
     def get_profile_image_filename(self):
-        return str(self.profile_image)[str(self.profile_image).index('profile_images/' + str(str.pk) + '/'):]
+        return str(self.profile_image)[str(self.profile_image).index('profile_images/' + str(self.pk) + '/'):]
 
     def has_perm(self, perm, obj=None):
         return self.is_staff
